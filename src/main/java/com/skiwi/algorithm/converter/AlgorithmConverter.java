b1a5f4cd6790055a928797cc92cdba909d50a9cd
skiwi <frank_skiwi@hotmail.com>
2014-11-12 21:53:22 +0100

    Added template Main.fxml file.

---
+++ src/main/java/com/skiwi/algorithm/converter/AlgorithmConverter.java
@@ -0,0 +1,28 @@
+package com.skiwi.algorithm.converter;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+final public class AlgorithmConverter extends Application {
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Algorithm Converter");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/skiwi/algorithm/converter/MainController.java
@@ -0,0 +1,22 @@
+package com.skiwi.algorithm.converter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Frank van Heeswijk
+ */
+public class MainController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
